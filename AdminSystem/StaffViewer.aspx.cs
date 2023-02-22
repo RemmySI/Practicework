f4f359b11a8fc948289e764c77ede8d0ff1c074e
Gabrielius Vaitkus <P2677845@my365.dmu.ac.uk>
2023-02-22 09:29:47 +0000

    Simple presentation layer

--- AdminSystem/StaffViewer.aspx.cs
+++ AdminSystem/StaffViewer.aspx.cs
@@ -4,11 +4,19 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1Viewer : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        //Create new instance
+        staffNo AnStaffNo = new staffNo();
 
+        //Get data from the session object
+        AnStaffNo = (staffNo)Session["AnStaffNo"];
+
+        //display the name
+        Response.Write(AnStaffNo.staffName);
     }
 }
