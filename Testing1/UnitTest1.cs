a21d4c4d3cf7188351ac62f51c41f18b7c78ef5c
Mohammed Ahiam <mohammed.aniam99@gmail.com>
2023-02-23 09:39:49 +0000

    Done all unit tests for Customer

--- Testing1/UnitTest1.cs
+++ Testing1/UnitTest1.cs
@@ -15,5 +15,55 @@ namespace Testing1
             Assert.IsNotNull(ACustomer);
 
         }
+        [TestMethod]
+        public void DateAddedCustomerOK()
+        {
+            clsCustomer ACustomer = new clsCustomer();
+            DateTime TestData = DateTime.Now.Date;
+            ACustomer.DateAdded = TestData;
+            Assert.AreEqual(ACustomer.DateAdded, TestData);
+        }
+        [TestMethod]
+        public void CustomerNoOK()
+        {
+            clsCustomer ACustomer = new clsCustomer();
+            Int32 TestData = 1;
+            ACustomer.CustomerNo = TestData;
+            Assert.AreEqual(ACustomer.CustomerNo, TestData);
+        }
+        [TestMethod]
+        public void CustomerFirstNameOk()
+        {
+            clsCustomer ACustomer = new clsCustomer();
+            string TestData = "Sam";
+            ACustomer.FirstName = TestData;
+            Assert.AreEqual(ACustomer.FirstName, TestData);
+        }
+        [TestMethod]
+        public void CustomerLastNameOk()
+        {
+            clsCustomer ACustomer = new clsCustomer();
+            string TestData = "Smith";
+            ACustomer.LastName = TestData;
+            Assert.AreEqual(ACustomer.LastName, TestData);
+        }
+        [TestMethod]
+        public void CustomerEmailAddressOK()
+        {
+            clsCustomer ACustomer = new clsCustomer();
+            string TestData = "ss@hotm.com";
+            ACustomer.Email = TestData;
+            Assert.AreEqual(ACustomer.Email, TestData);
+        }
+        [TestMethod]
+        public void CustomerPhoneNoOK()
+        {
+            clsCustomer ACustomer = new clsCustomer();
+            string TestData = "2151254207";
+            ACustomer.PhoneNo = TestData;
+            Assert.AreEqual(ACustomer.PhoneNo, TestData);
+        }
     }
 }
+
+
--- Testing1/clsCustomer.cs
+++ Testing1/clsCustomer.cs
@@ -1,6 +1,14 @@
-﻿namespace Testing1
+﻿using System;
+
+namespace Testing1
 {
     class clsCustomer
     {
+        public string PhoneNo { get; internal set; }
+        public string Email { get; internal set; }
+        public string LastName { get; internal set; }
+        public DateTime DateAdded { get; internal set; }
+        public int CustomerNo { get; internal set; }
+        public string FirstName { get; internal set; }
     }
 }
