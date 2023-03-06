fab5102ac3aa1d08d17f7f5c452b0ca8f2ada161
P2422047 <P2422047@my365.dmu.ac.uk>
2023-03-06 18:38:39 +0000

    Added testing for address class

---
+++ Testing4/tstStock.cs
@@ -0,0 +1,20 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing4
+{
+    [TestClass]
+    public class tstStock
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //Create an instance of the class we want to create
+            clsStock AnStock = new clsStock();
+            //test to see that it exists
+            Assert.IsNotNull(AnStock);
+
+        }
+    }
+}
