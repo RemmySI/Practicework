0aacbb5043454322342fba29842c872477c535c4
rdkang <p2689814@my365.dmu.ac.uk>
2023-04-16 19:57:52 +0100

    feat: added first order test
    Week 19

---
+++ Testing2/tstOrder.cs
@@ -0,0 +1,18 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing2
+{
+    [TestClass]
+    public class tstOrder
+    {
+        [TestMethod]
+        public void InstanceOk()
+        {
+            // tests to see if initialising class exists and works
+            clsOrder AnOrder = new clsOrder();
+            Assert.IsNotNull(AnOrder);
+        }
+    }
+}
