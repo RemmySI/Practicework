e5a2924524607156d3eadb6151394c6f297e968b
rdkang <p2689814@my365.dmu.ac.uk>
2023-04-17 12:19:46 +0100

    feat: added more tests for Orders

--- ClassLibrary/clsOrder.cs
+++ ClassLibrary/clsOrder.cs
@@ -1,6 +1,14 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsOrder
     {
+        public bool Delivered { get; set; }
+        public int Quantity { get; set; }
+        public DateTime OrderDate { get; set; }
+        public int OrderNo { get; set; }
+        public string DeliveryAddress { get; set; }
+        public decimal Price { get; set; }
     }
 }
--- Testing2/tstOrder.cs
+++ Testing2/tstOrder.cs
@@ -14,5 +14,66 @@ namespace Testing2
             clsOrder AnOrder = new clsOrder();
             Assert.IsNotNull(AnOrder);
         }
+        [TestMethod]
+        public void OrderNoOk()
+        {
+            clsOrder AnOrder = new clsOrder();
+            int TestData = 1;
+            AnOrder.OrderNo = 1;
+            Assert.AreEqual(AnOrder.OrderNo, TestData);
+        }
+
+        [TestMethod]
+        public void DeliveredOk()
+        {
+            clsOrder AnOrder = new clsOrder();
+            Boolean TestData = true;
+            AnOrder.Delivered = TestData;
+            Assert.AreEqual(AnOrder.Delivered, TestData);
+        }
+
+        [TestMethod]
+        public void QuantityOk()
+        {
+            clsOrder AnOrder = new clsOrder();
+            int TestData = 5;
+            AnOrder.Quantity = TestData;
+            Assert.AreEqual(AnOrder.Quantity, TestData);
+
+        }
+        [TestMethod]
+        public void DeliveryAddressOk()
+        {
+            clsOrder AnOrder = new clsOrder();
+            String TestData = "69 window road";
+            AnOrder.DeliveryAddress = TestData;
+            Assert.AreEqual(AnOrder.DeliveryAddress, TestData);
+
+        }
+
+        [TestMethod]
+        public void OrderDateOK()
+        {
+            //create an instance of the class we want to create
+            clsOrder AnOrder = new clsOrder();
+            //create some test data to assign to the Item
+            DateTime TestData = DateTime.Now.Date;
+            //Assign the data to the item
+            AnOrder.OrderDate = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AnOrder.OrderDate, TestData);
+        }
+        [TestMethod]
+        public void PriceOk()
+        {
+            //create an instance of the class we want to create
+            clsOrder AnOrder = new clsOrder();
+            //create some test data to assign to the Item
+            decimal TestData = new decimal(4.2);
+            //Assign the data to the item
+            AnOrder.Price = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AnOrder.Price, TestData);
+        }
     }
 }
