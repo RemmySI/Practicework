2f893c47327388f2fe4c6501f41d7925b5df536b
GabrieliusVaitkus <98562855+GabrieliusVaitkus@users.noreply.github.com>
2023-04-19 10:28:11 +0100

    Revert "Progress"

--- ClassLibrary/clsStock.cs
+++ ClassLibrary/clsStock.cs
@@ -4,46 +4,11 @@ namespace ClassLibrary
 {
     public class clsStock
     {
-        private Int32 mStockNo;
         public bool Available { get; set; }
         public decimal CardPrice { get; set; }
         public string CardType { get; set; }
         public string CardDescription { get; set; }
-        public int CardNo 
-        {
-            get
-            {
-                return mStockNo;
-            }
-            set {
-                mStockNo = value;
-            }
-        }
-        public DateTime DateAdded
-        {
-            get
-            {
-                return mDateAdded;
-            }
-            set
-            {
-                mDateAdded = value;
-            }
-        }
-
-
-
-        public bool Find(int CardNo)
-        {
-            mDateAdded = Convert.ToDateTime("16/9/2015");
-            mStockNo = 21;
-            //always return true;
-            return true;
-        }
-
-    
-
-
-
+        public int CardNo { get; set; }
+        public DateTime DateAdded { get; set; }
     }
 }
