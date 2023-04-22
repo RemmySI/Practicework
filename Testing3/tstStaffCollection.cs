356fdfad8b25d5d383aeb5c611087a1e85c5c9f6
Gabrielius Vaitkus <P2677845@my365.dmu.ac.uk>
2023-04-22 09:48:24 +0100

    Creating the Collection Class finished

--- Testing3/tstStaffCollection.cs
+++ Testing3/tstStaffCollection.cs
@@ -33,5 +33,41 @@ namespace Testing3
             AllStaff.StaffList = TestList;
             Assert.AreEqual(AllStaff.StaffList, TestList);
         }
+
+        [TestMethod]
+        public void ThisStaffPropertyOK()
+        {
+            clsStaffCollection AllStaff = new clsStaffCollection();
+            clsStaff TestAddress = new clsStaff();
+            TestAddress.staffNo = 2;
+            TestAddress.staffName = "John Smith";
+            TestAddress.staffAddress = "Mangrove 1";
+            TestAddress.staffMoNumber = "7845123456";
+            TestAddress.staffRole = "Manager";
+            TestAddress.staffBirthDate = DateTime.Parse("18/08/1998");
+            TestAddress.staffActive = true;
+
+            AllStaff.ThisStaff = TestAddress;
+            Assert.AreEqual(AllStaff.ThisStaff, TestAddress);
+        }
+
+        [TestMethod]
+        public void StaffListAndCountOK()
+        {
+            clsStaffCollection AllStaff = new clsStaffCollection();
+            List<clsStaff> TestList = new List<clsStaff>();
+            clsStaff TestItem = new clsStaff();
+            TestItem.staffNo = 2;
+            TestItem.staffName = "John Smith";
+            TestItem.staffAddress = "Mangrove 1";
+            TestItem.staffMoNumber = "7845123456";
+            TestItem.staffRole = "Manager";
+            TestItem.staffBirthDate = DateTime.Parse("18/08/1998");
+            TestItem.staffActive = true;
+
+            TestList.Add(TestItem);
+            AllStaff.StaffList = TestList;
+            Assert.AreEqual(AllStaff.Count, TestList.Count);
+        }
     }
 }
